152e4d64fd8f155e6d46ed066936d2630c1e13a3
AiorNerok <90855034+AiorNerok@users.noreply.github.com>
2022-12-08 20:30:03 +0300

    Add files via upload

---
+++ parse.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from tabulate import tabulate
+
+MAIN_URL = "https://www.python.org"
+
+ua = UserAgent()
+
+headers = {"user-agent": ua.random}
+
+response = requests.get(MAIN_URL, headers=headers)
+
+soup = BeautifulSoup(response.content, features="html.parser")
+
+events_block = soup.find(class_="medium-widget event-widget last")
+events_menu = events_block.find_all("li")
+
+parse_events_list = []
+
+for i in events_menu:
+    _, event_data, event_name = i.text.split("\n")
+    event_link = MAIN_URL + i.find("a")["href"]
+    parse_events_list.append([event_data, event_name, event_link])
+
+print(tabulate(parse_events_list, headers=["Date", "Name", "Link"], tablefmt="github"))
